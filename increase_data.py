898a181d55ec1b406f4bf61cb8e67be2e68005c8
sp <zoneofatombomb@gmail.com>
2018-04-11 23:12:35 +0300

    init

---
+++ increase_data.py
@@ -0,0 +1,17 @@
+from keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   shear_range=0.1,
+                                   zoom_range=0.1,
+                                   rotation_range=270,
+                                   horizontal_flip=True
+                                   )
+i = 0
+for training_set in \
+        train_datagen.flow_from_directory('dataset/training_set',
+                                          target_size=(182, 182),
+                                          batch_size=1000,
+                                          class_mode="binary",
+                                          save_to_dir='dataset/training_set'):
+    i += 1
+    if i == 1:
+        break
